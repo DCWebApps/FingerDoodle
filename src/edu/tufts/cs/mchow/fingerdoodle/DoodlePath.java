5ce59c1126a038dab3b688c6c213dcca4ec37062
Ming Chow <mchow@cs.tufts.edu>
2013-03-20 16:06:09 -0400

    Renamed application package (source) to edu.tufts.cs.mchow.fingerdoodle

---
+++ src/edu/tufts/cs/mchow/fingerdoodle/DoodlePath.java
@@ -0,0 +1,17 @@
+package edu.tufts.cs.mchow.fingerdoodle;
+
+import android.graphics.*;
+
+public class DoodlePath
+{
+	protected Paint paint;
+	protected Path path;
+	protected boolean eraser;
+	
+	public DoodlePath (Path path, Paint p, boolean eraser)
+	{
+		this.path = path;
+		this.paint = p;
+		this.eraser = eraser;
+	}
+}
